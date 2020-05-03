6b482f34657181add3fc56d10c80f86c75a977e8
David Earle <djearle98@icloud.com>
2020-05-03 02:39:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,15 +5,46 @@ var stage = document.getElementById("stage");
 addNode();
 
 function click(){
-	var node = document.getElementById("svg").setAttribute("fill", "green");
+	console.log("CLICKED");
+	var node = document.getElementsByClassName("node")[0];
+  var root = document.documentElement;
+  var curColor = root.style.getPropertyValue("--node-color");
+  
+  if (curColor == "green"){
+  	root.style.setProperty("--node-color", "blue");
+	  root.style.setProperty("--pos-x", "50px");
+	  root.style.setProperty("--pos-y", "50px");
+
+  } else {
+  	root.style.setProperty("--node-color", "green");
+	  root.style.setProperty("--pos-x", "100px");
+	  root.style.setProperty("--pos-y", "100px");
+  }
 }
 
 function addNode(){
-	var node = document.createElementNS(SVG_NS, "circle");
-  node.setAttribute("r", "30");
-  node.setAttribute("cx", "30");
-  node.setAttribute("cy", "30");
-  node.setAttribute("id", "svg");
-  node.addEventListener("click", click);
-  stage.appendChild(node);
+	var nodeContainer = document.createElementNS(SVG_NS, "g");
+  nodeContainer.setAttribute("class", "node-container");
+
+  var node = document.createElementNS(SVG_NS, "svg");
+  node.setAttribute("class", "node");
+  node.setAttribute("width", "150");
+  node.setAttribute("height", "150");
+	
+  var dev_outline = document.createElementNS(SVG_NS, "rect");
+  dev_outline.setAttribute("class", "dev_outline");
+  dev_outline.setAttribute("width", "100%");
+  dev_outline.setAttribute("height", "100%");
+
+  var background = document.createElementNS(SVG_NS, "circle");
+  background.addEventListener("click", click);	
+  background.setAttribute("r", "50%");
+  background.setAttribute("cx", "50%");
+  background.setAttribute("cy", "50%");
+  background.setAttribute("class", "node_background");
+
+  //node.appendChild(dev_outline);
+  node.appendChild(background);
+  nodeContainer.appendChild(node);
+  stage.appendChild(nodeContainer);
 }
