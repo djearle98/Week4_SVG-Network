88af7e9c1aa9f96542a633b300c21ec3f50ad62b
David Earle <djearle98@icloud.com>
2020-05-06 01:24:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var SVG_NS = 'http://www.w3.org/2000/svg';
 var XLink_NS = 'http://www.w3.org/1999/xlink';
-var nodes = [];
-var lines = [];
+var graphicNodes = [];
+var graphicLines = [];
+var dataNodes = new Map();
 var stage = document.getElementById("stage");
 
 var lineLayer = document.createElementNS(SVG_NS, "svg");
@@ -18,7 +19,7 @@ nodeLayer_bg.setAttribute("class","node-layer_bg");
 nodeLayer.appendChild(nodeLayer_bg);
 stage.appendChild(nodeLayer);
 
-class Node {
+class GraphicNode {
 	constructor(title,x,y,r) {
   	//CREATE ELEMENTS OF NODE
     //node container, a group for transformations
@@ -30,18 +31,33 @@ class Node {
     this.node.setAttribute("class", "node");
     this.radius = r;
 		this.coordinates = ([x, y]);
-		
+    this.shadowOffset = 6;
+    
+    //shadow, the visible shadow of the node
+    this.shadow = document.createElementNS(SVG_NS, "circle");
+    this.shadow.setAttribute("r", r);
+    this.shadow.setAttribute("cx", r+this.shadowOffset);
+    this.shadow.setAttribute("cy", r+this.shadowOffset);
+    this.shadow.setAttribute("class", "node_shadow");
+    
     //background, the visible circle of the node
     this.background = document.createElementNS(SVG_NS, "circle");
-    this.background.setAttribute("r", "50%");
-    this.background.setAttribute("cx", "50%");
-    this.background.setAttribute("cy", "50%");
+    this.background.setAttribute("r", r);
+    this.background.setAttribute("cx", r);
+    this.background.setAttribute("cy", r);
     this.background.setAttribute("class", "node_background");
 		
+    //status ring, the circlular color indicator
+    this.statusRing = document.createElementNS(SVG_NS, "circle");
+    this.statusRing.setAttribute("r", r-5);
+    this.statusRing.setAttribute("cx", r);
+    this.statusRing.setAttribute("cy", r);
+    this.statusRing.setAttribute("class", "node_status-ring");
+    
     //title, the node's title field
     this.title = document.createElementNS(SVG_NS, "text");
-    this.title.setAttribute("x", "50%");
-    this.title.setAttribute("y", "50%");
+    this.title.setAttribute("x", r);
+    this.title.setAttribute("y", r+5);
     this.title.setAttribute("text-anchor", "middle");
     this.title.setAttribute("class", "title");
     //titleText, the text in the title
@@ -49,10 +65,10 @@ class Node {
     
     //blanket, a circlular transparent layer atop the entire node just for detecting click events
     this.blanket = document.createElementNS(SVG_NS, "circle");
-    this.blanket.addEventListener("click", Node.demoMove.bind(this));	
-    this.blanket.setAttribute("r", "50%");
-    this.blanket.setAttribute("cx", "50%");
-    this.blanket.setAttribute("cy", "50%");
+    this.blanket.addEventListener("click", GraphicNode.demoMove.bind(this));	
+    this.blanket.setAttribute("r", r);
+    this.blanket.setAttribute("cx", r);
+    this.blanket.setAttribute("cy", r);
 		this.blanket.setAttribute("class", "blanket");
 		
     //ADD ELEMENTS TO DOCUMENT
@@ -60,7 +76,9 @@ class Node {
     this.title.appendChild(this.titleText);
     
     //add shapes to node
+    this.node.appendChild(this.shadow);
     this.node.appendChild(this.background);
+    this.node.appendChild(this.statusRing);
     this.node.appendChild(this.title);
     this.node.appendChild(this.blanket);
     
@@ -72,8 +90,8 @@ class Node {
   }
   set radius(r){
   	this.r = r;
-  	this.node.setAttribute("width", r*2);
-    this.node.setAttribute("height", r*2);
+  	this.node.setAttribute("width", r*2+this.shadowOffset);
+    this.node.setAttribute("height", r*2+this.shadowOffset);
   }
   get radius() {
   	return this.r;
@@ -94,7 +112,7 @@ class Node {
     this.coordinates=c;
   }
 }
-class Line{
+class GraphicLine{
 	constructor(parent, child){
   	this.parent = parent;
     this.child = child;
@@ -109,35 +127,24 @@ class Line{
     this.line.setAttribute("y2", cc[1]);
     lineLayer.appendChild(this.line);
   }
-  update(){ //TODO: DOESN'T WORK
-  	//Uncaught DOMException: Blocked a frame with origin "https://fiddle.jshell.net" from accessing a cross-origin frame.
-
-  	let pc = parent.coordinates;
-    let cc = child.coordinates;
-  	this.line.setAttribute("x1", pc[0]);
-    this.line.setAttribute("y1", pc[1]);
-  	this.line.setAttribute("x2", cc[0]);
-    this.line.setAttribute("y2", cc[1]);
-  }
 }
 
-nodes.push(new Node("NODE 1", 130, 153, 196/2));
-nodes.push(new Node("NODE 2", 220, 373, 118/2));
-nodes.push(new Node("NODE 3", 435, 381, 174/2));
-nodes.push(new Node("NODE 4", 372, 169, 138/2));
-nodes.push(new Node("NODE 5", 353, 553, 118/2));
-nodes.push(new Node("NODE 6", 546, 553, 118/2));
-nodes.push(new Node("NODE 7", 640, 383, 118/2));
-nodes.push(new Node("NODE 8", 555, 243, 118/2));
-nodes.push(new Node("NODE 9", 555, 94, 118/2));
+graphicNodes.push(new GraphicNode("NODE 1", 130, 153, 196/2));
+graphicNodes.push(new GraphicNode("NODE 2", 220, 373, 118/2));
+graphicNodes.push(new GraphicNode("NODE 3", 435, 381, 174/2));
+graphicNodes.push(new GraphicNode("NODE 4", 372, 169, 138/2));
+graphicNodes.push(new GraphicNode("NODE 5", 353, 553, 118/2));
+graphicNodes.push(new GraphicNode("NODE 6", 546, 553, 118/2));
+graphicNodes.push(new GraphicNode("NODE 7", 640, 383, 118/2));
+graphicNodes.push(new GraphicNode("NODE 8", 555, 243, 118/2));
+graphicNodes.push(new GraphicNode("NODE 9", 555, 94, 118/2));
 
-lines.push(new Line(nodes[0],nodes[1]));
-lines.push(new Line(nodes[0],nodes[2]));
-lines.push(new Line(nodes[0],nodes[3]));
-lines.push(new Line(nodes[3],nodes[7]));
-lines.push(new Line(nodes[3],nodes[8]));
-lines.push(new Line(nodes[2],nodes[4]));
-lines.push(new Line(nodes[2],nodes[5]));
-lines.push(new Line(nodes[2],nodes[6]));
-
-//lines[0].update(); //throws error
+graphicLines.push(new GraphicLine(graphicNodes[0],graphicNodes[1]));
+graphicLines.push(new GraphicLine(graphicNodes[0],graphicNodes[2]));
+graphicLines.push(new GraphicLine(graphicNodes[0],graphicNodes[3]));
+graphicLines.push(new GraphicLine(graphicNodes[3],graphicNodes[7]));
+graphicLines.push(new GraphicLine(graphicNodes[3],graphicNodes[8]));
+graphicLines.push(new GraphicLine(graphicNodes[2],graphicNodes[4]));
+graphicLines.push(new GraphicLine(graphicNodes[2],graphicNodes[5]));
+graphicLines.push(new GraphicLine(graphicNodes[2],graphicNodes[6]));
+graphicLines.push(new GraphicLine(graphicNodes[3],graphicNodes[2]));
